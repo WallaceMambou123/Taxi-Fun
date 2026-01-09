03f7b5d08aac537be3e060635da94442931934b5
Yann <yannngolamze@gmail.com>
2026-01-09 13:34:22 +0100

    correctif

--- taxifun-backend/src/auth/auth.module.ts
+++ taxifun-backend/src/auth/auth.module.ts
@@ -18,7 +18,7 @@ import { JwtStrategy } from './jwt.strategy';
       useFactory: (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
         signOptions: {
-          expiresIn: configService.get<string>('JWT_EXPIRES_IN') || '7d',
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN') || '1h',
         },
       }),
       inject: [ConfigService],
